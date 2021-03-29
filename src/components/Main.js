fbd7f5d94fcdb734b26d7d5e424265fce9b55b62
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-03-30 07:26:18 +0900

    profile作成

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,40 @@ import { ApiContext } from "../context/ApiContext";
 import Grid from "@material-ui/core/Grid";
 import { GoMail } from "react-icons/go";
 import { BsFillPeopleFill } from "react-icons/bs";
+import Profile from "./Profile";
+import ProfileManager from "./ProfileManager";
 
 const Main = () => {
+    const { profiles, profile, askList, askListFull, inbox } = useContext(
+        ApiContext
+        );
+        const filterProfiles = profiles.filter((prof) => {
+        return prof.id !== profile.id;
+        });
+        const listProfiles =
+        filterProfiles &&
+        filterProfiles.map((filprof) => (
+            <Profile
+            key={filprof.id}
+            profileData={filprof}
+            askData={askListFull.filter((ask) => {
+                return (
+                (filprof.userPro === ask.askFrom) | (filprof.userPro === ask.askTo)
+                );
+            })}
+            />
+        ));
   return (
     <Grid container>
       <Grid item xs={4}>
         <div className="app-profiles">
+          {listProfiles}
         </div>
       </Grid>
 
       <Grid item xs={4}>
         <div className="app-details">
+          <ProfileManager />
         </div>
         <h3 className="title-ask">
           <BsFillPeopleFill className="badge" />
